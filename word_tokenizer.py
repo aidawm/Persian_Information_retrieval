874476656225cbc531315828a4e63d176c7a0fc7
aida.mobli <aidaw.mobli@gmail.com>
2024-01-23 22:45:59 +0330

    update tokenizer

--- word_tokenizer.py
+++ word_tokenizer.py
@@ -10,24 +10,15 @@ class WordTokenizer:
 
         return tokens
     
-
-
-    def delete_punctuations_symbols(self,text):
-        punctuations = [".","،",",","!","?","\"","\'","(",")","{","}","[","]","/","\\",":"]
-        for p in punctuations:
-            text = text.replace(p,"")
-
-        return text
-    
-
-    def process_verbs(self,token: list[str]):
+    def process_verbs(self,tokens: list[str]):
         del_index = []
-        for i,t in enumerate(token):
-            if t == "می":
-                token[i+1] = "می"+ token[i+1]
+        for i,t in enumerate(tokens):
+            if t == "می" or t == "نمی": 
+                mi = t+ "\u200c"
+                tokens[i+1] = mi+ tokens[i+1]
                 del_index.append(i)
 
         for i in range(len(del_index)-1,-1,-1):
-            token.pop(del_index[i])
+            tokens.pop(del_index[i])
         
-        return token
+        return tokens
